c78818e21b54987fc6feba44f77d506aa6dce74c
Thaynara <104026459+thaynara15@users.noreply.github.com>
2023-02-15 11:36:43 -0300

    Add files via upload

---
+++ js.javascript.js
@@ -0,0 +1,32 @@
+var gallery = [
+    {
+      src: "https://lipsum.app/id/61/1800x1200",
+      thumb: "https://lipsum.app/id/2/80x80",
+      caption: "First image",
+    },
+    {
+      src: "https://lipsum.app/id/3/800x600",
+      thumb: "https://lipsum.app/id/3/80x80",
+      caption: "Second image",
+    },
+    {
+      src: "https://lipsum.app/id/4/800x600",
+      thumb: "https://lipsum.app/id/4/80x80",
+      caption: "Third image",
+    },
+    {
+      src: "https://lipsum.app/id/4/800x600",
+      thumb: "https://lipsum.app/id/4/80x80",
+      caption: "Third image",
+    },
+
+    <head>
+        <body>
+        <div id= "seta"></div>
+    </body>
+    </head>
+
+    
+  ];
+
+  
